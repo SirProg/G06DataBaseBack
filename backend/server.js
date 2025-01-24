074705e78ffbf1c8e9341e2327a246b87454e6b1
SirProg <kevinmaldonadoparedes@gmail.com>
2025-01-24 15:46:04 -0500

    ADD the CRUD to the table calzados

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,8 @@ const connection = mysql.createConnection({
 });
 
 
-const dataCalzados = app.get('/api/calzados', (req, res) => {
-    const sql = 'SELECT * FROM Calzados';
+app.get('/api/calzados', (req, res) => {
+    const sql = 'SELECT * FROM Calzado';
     connection.query(sql, (error, results) => {
         if (error) {
             res.send('Error al obtener los calzados');
@@ -26,7 +26,46 @@ const dataCalzados = app.get('/api/calzados', (req, res) => {
     });
 });
 
-console.log(dataCalzados)
+app.post("/api/addCalzado", (req, res) => {
+    const {id, talla, color, precio, tipo, nombre, existencias} = req.body;
+    connection.query(
+        "INSERT INTO Calzado (IDCalzado, Talla, Color, Precio, Tipo, Nombre, ExistenciasActuales) values(?, ?, ?, ?, ?, ?, ?)",
+        [id, talla, color, precio, tipo, nombre, existencias],
+        (err, result) => {
+            if(err){
+                res.status(500).json({error: "Error para crear los calzados"});
+                return;
+            }
+            res.status(200).json({message: "Calzado creado exitosamente"});
+        });
+});
+
+app.put('/api/updateCalzado/:id', (req, res) => {
+    const {id} = req.params;
+    const {talla, color, precio, tipo, nombre, existencias} = req.body;
+    connection.query(
+        'UPDATE Calzado SET Talla = ?, Color = ?, Precio = ?, Tipo = ?, Nombre = ?, ExistenciasActuales = ? WHERE IDCalzado = ?',
+        [talla, color, precio, tipo, nombre, existencias, id],
+        (err)=>{
+            if(err){
+                res.status(500).json({error: "Error para actualizar el calzado"});
+            }
+            res.json({id, talla, color, precio, tipo, nombre, existencias});
+        });
+});
+
+app.delete('/api/deleteCalzado/:id',(req, res) =>{
+    const {id} = req.params;
+    connection.query(
+        'DELETE FROM Calzado WHERE IDCalzado = ?',
+        [id],
+        (err)=>{
+            if(err){
+                res.status(500).json({error: "Error para eliminar el calzado"});
+            }
+            res.json({message: "Calzado eliminado exitosamente"});
+        });
+});
 
 connection.connect((err) =>{
     if(err){
